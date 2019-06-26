c9449de20418c62199d417f9dc03560fc45cce8d
vwilkins1 <vannessa.wilk@gmail.com>
2019-06-26 01:46:09 -0400

    Update data.js
    Capitalize menu items

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
-let croqueta = new MenuItem('fusion croqueta', 325)
-let reuben = new Sandwich('hipster reuben', 1345)
-let club = new Sandwich('SoBe club', 1099)
+let croqueta = new MenuItem('Fusion Croqueta', 325)
+let reuben = new Sandwich('Hipster Reuben', 1345)
+let club = new Sandwich('SoBe Club', 1099)
 let italian = new Sandwich('Italian', 950)
 
 let menuItems = [ croqueta, reuben, club, italian ]
